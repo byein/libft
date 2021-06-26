9203ea198f062372fe0eca20014bbf3d657e3079
Yebin Kim <yebkim@c8r2s10.42seoul.kr>
2021-06-26 18:02:22 +0900

    add condition

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: yebkim <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/12 11:46:43 by yebkim            #+#    #+#             */
-/*   Updated: 2021/06/26 17:40:01 by yebkim           ###   ########.fr       */
+/*   Updated: 2021/06/26 17:51:56 by yebkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n)
 	i = 0;
 	while (i < n - 1 && s1[i] && s2[i] && s1[i] == s2[i])
 		i++;
-	if (n == 0)
+	if (n == 0 || s1[i] == s2[i])
 		return (0);
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
